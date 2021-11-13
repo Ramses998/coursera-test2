0f1a295f0bb4256801e039b1f6239b27ad93088f
Ramses998 <84883418+Ramses998@users.noreply.github.com>
2021-11-12 18:26:40 -0800

    mensaje

---
+++ module4-solution/src/items/items.controller.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    angular.module('MenuApp')
+        .controller('itemsCtrl', itemsCtrl);
+    itemsCtrl.$inject = ['items']
+    function itemsCtrl(items) {
+        let ctrl = this;
+        ctrl.items = items['menu_items'];
+    }
+}());
